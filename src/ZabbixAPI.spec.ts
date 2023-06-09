641444bed724192bc43f5e5b1d70a8889b59ebd1
Alexander Radyushin <a.radyushin@pm.me>
2023-06-09 03:53:37 -0300

    fix(deps): bump all deps, update tsconfig

--- src/ZabbixAPI.spec.ts
+++ src/ZabbixAPI.spec.ts
@@ -1,57 +1,61 @@
-import { ZabbixCommunicator } from './ZabbixCommunicator';
-import { ZabbixSocket } from './ZabbixSocket';
-import { ZabbixAPI } from './ZabbixAPI';
+import { ZabbixCommunicator } from "./ZabbixCommunicator";
+import { ZabbixSocket } from "./ZabbixSocket";
+import { ZabbixAPI } from "./ZabbixAPI";
 
-describe('ZabbixAPI', () => {
-    afterEach(() => {
-        jest.clearAllMocks();
+describe("ZabbixAPI", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should try to login", async () => {
+    const response = "someresponse";
+    const username = "someusername";
+    const password = "somepass";
+
+    const callSpy = jest
+      .spyOn(ZabbixCommunicator.prototype, "call")
+      .mockResolvedValue(response);
+
+    const socket = new ZabbixSocket("");
+    const api = new ZabbixAPI(socket);
+
+    const methodSpy = jest.spyOn(api, "method");
+
+    const res = await api.login(username, password);
+
+    expect(callSpy).toBeCalledWith({
+      username,
+      password,
     });
+    expect(methodSpy).toBeCalledWith("user.login");
+    expect(res).toBe(response);
+  });
 
-    it('should try to login', async () => {
-        const response = 'someresponse';
-        const username = 'someusername';
-        const password = 'somepass';
+  it("should try to logout", async () => {
+    const response = "someresponse";
 
-        const callSpy = jest.spyOn(ZabbixCommunicator.prototype, 'call').mockResolvedValue(response);
+    const callSpy = jest
+      .spyOn(ZabbixCommunicator.prototype, "call")
+      .mockResolvedValue(response);
 
-        const socket = new ZabbixSocket('');
-        const api = new ZabbixAPI(socket);
+    const socket = new ZabbixSocket("");
+    const api = new ZabbixAPI(socket);
 
-        const methodSpy = jest.spyOn(api, 'method');
+    const methodSpy = jest.spyOn(api, "method");
 
-        const res = await api.login(username, password);
+    const res = await api.logout();
 
-        expect(callSpy).toBeCalledWith({
-            user: username,
-            password,
-        });
-        expect(methodSpy).toBeCalledWith('user.login');
-        expect(res).toBe(response);
-    });
+    expect(callSpy).toBeCalledTimes(1);
+    expect(methodSpy).toBeCalledWith("user.logout");
+    expect(res).toBe(response);
+  });
 
-    it('should try to logout', async () => {
-        const response = 'someresponse';
+  it("should return a ZabbixCommunicator when defining a method to use", () => {
+    const socket = new ZabbixSocket("");
+    const api = new ZabbixAPI(socket);
 
-        const callSpy = jest.spyOn(ZabbixCommunicator.prototype, 'call').mockResolvedValue(response);
+    const comm = api.method("any.method");
 
-        const socket = new ZabbixSocket('');
-        const api = new ZabbixAPI(socket);
-
-        const methodSpy = jest.spyOn(api, 'method');
-
-        const res = await api.logout();
-
-        expect(callSpy).toBeCalledTimes(1);
-        expect(methodSpy).toBeCalledWith('user.logout');
-        expect(res).toBe(response);
-    });
-
-    it('should return a ZabbixCommunicator when defining a method to use', () => {
-        const socket = new ZabbixSocket('');
-        const api = new ZabbixAPI(socket);
-
-        const comm = api.method('any.method');
-
-        expect(comm).toBeInstanceOf(ZabbixCommunicator);
-    });
+    expect(comm).toBeInstanceOf(ZabbixCommunicator);
+  });
 });
