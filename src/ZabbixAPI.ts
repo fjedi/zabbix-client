6dac3f58d5545c918834e3b0de8d17944cdfbe24
Alexander Radyushin <a.radyushin@pm.me>
2023-06-09 03:41:13 -0300

    fix(auth): pass 'username' param instead of 'user' (breaking change in zabbix-server 6.4)

--- src/ZabbixAPI.ts
+++ src/ZabbixAPI.ts
@@ -1,61 +1,61 @@
-import { ZabbixSocket } from './ZabbixSocket';
-import { ZabbixCommunicator } from './ZabbixCommunicator';
-import { AxiosInstance } from 'axios';
+import { ZabbixSocket } from "./ZabbixSocket";
+import { ZabbixCommunicator } from "./ZabbixCommunicator";
+import { AxiosInstance } from "axios";
 
 /**
  * The API link which can login, logout and set a method to use
  */
 export class ZabbixAPI {
-    private socket: ZabbixSocket;
+  private socket: ZabbixSocket;
 
-    private reloginInterceptor: number;
+  private reloginInterceptor: number;
 
-    constructor(socket: ZabbixSocket) {
-        this.socket = socket;
+  constructor(socket: ZabbixSocket) {
+    this.socket = socket;
+  }
+
+  public getHttpSocket(): AxiosInstance {
+    return this.socket.getHttp();
+  }
+
+  public getSocket(): ZabbixSocket {
+    return this.socket;
+  }
+
+  public setReloginInterceptor(interceptor: number) {
+    this.reloginInterceptor = interceptor;
+  }
+
+  /**
+   * Login and set the access token for further api calls
+   * @param username
+   * @param password
+   */
+  public async login(username: string, password: string): Promise<string> {
+    return await this.method("user.login").call({
+      username,
+      password,
+    });
+  }
+
+  /**
+   * Logout and unset the access token, no api call can be made
+   * without logging again
+   */
+  public async logout() {
+    const result = await this.method("user.logout").call([]);
+    this.socket.setToken(null);
+    if (this.reloginInterceptor) {
+      this.getHttpSocket().interceptors.request.eject(this.reloginInterceptor);
     }
+    return result;
+  }
 
-    public getHttpSocket(): AxiosInstance {
-        return this.socket.getHttp();
-    }
-
-    public getSocket(): ZabbixSocket {
-        return this.socket;
-    }
-
-    public setReloginInterceptor(interceptor: number) {
-        this.reloginInterceptor = interceptor;
-    }
-
-    /**
-     * Login and set the access token for further api calls
-     * @param username
-     * @param password
-     */
-    public async login(username: string, password: string): Promise<string> {
-        return await this.method('user.login').call({
-            user: username,
-            password,
-        });
-    }
-
-    /**
-     * Logout and unset the access token, no api call can be made
-     * without logging again
-     */
-    public async logout() {
-        const result = await this.method('user.logout').call([]);
-        this.socket.setToken(null);
-        if (this.reloginInterceptor) {
-            this.getHttpSocket().interceptors.request.eject(this.reloginInterceptor);
-        }
-        return result;
-    }
-
-    /**
-     * Set the zabbix method to use. Ex: 'host.get'
-     * @param method
-     */
-    public method(method: string): ZabbixCommunicator {
-        return new ZabbixCommunicator(this.socket, method);
-    }
+  /**
+   * Set the zabbix method to use. Ex: 'host.get'
+   * @param method
+   */
+  public method(method: string): ZabbixCommunicator {
+    return new ZabbixCommunicator(this.socket, method);
+  }
 }
